342176dce73c78918407c832a8f13f6f862ef2b1
Chitravansh <chitravanshmohandevelops@gmail.com>
2026-01-14 23:49:50 +0530

    first commit

---
+++ components/Home/Home.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className="overflow-hidden">
+      Home
+    </div>
+  )
+}
+
+export default Home
+
+
+//LocalHost:3000 
+// http://localhost:3000
+
+
+/*  For About page you neeed to go Localhost:3000/about
+    Localhost:3000/about/team
+    Localhost:3000/about/team/sajal
+ */
