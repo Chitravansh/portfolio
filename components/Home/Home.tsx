e744f76af564c08305fb6d3c08a86f4bdb2a0133
Chitravansh <chitravanshmohandevelops@gmail.com>
2026-01-16 00:47:35 +0530

    Added Hero Section

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Hero from './Hero/Hero'
 
 const Home = () => {
   return (
-    <div className="overflow-hidden">
-      Home
+    <div className="relative h-screen flex items-center justify-center text-white overflow-hidden flex-col">
+     <Hero />
     </div>
   )
 }
